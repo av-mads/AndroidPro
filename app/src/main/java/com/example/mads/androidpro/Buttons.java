3d3e10836ac95831a02e9eca7f49e5b83ae98365
MadsChris92 <madschris888@gmail.com>
2016-02-16 15:49:24 +0100

    das

--- app/src/main/java/com/example/mads/androidpro/Buttons.java
+++ app/src/main/java/com/example/mads/androidpro/Buttons.java
@@ -2,6 +2,10 @@ package com.example.mads.androidpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class Buttons extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class Buttons extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buttons);
+
+        ArrayList<Button> btns = new ArrayList<>();
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        Button btn3 = (Button) findViewById(R.id.btn3);
+
+        btns.add(0, btn1);
+        btns.add(1, btn2);
+        btns.add(2, btn3);
+
+        for (int i = 0; i < btns.size(); i++) {
+//            btns.get(i).setMinimumWidth(200);
+
+            btns.get(i).setMinWidth(200);
+            btns.get(i).setOnClickListener(clickListen(btns.get(i)));
+        }
+
+    }
+
+    View.OnClickListener clickListen(final Button button){
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                button.setText("BLA");
+            }
+        };
+
     }
 }
