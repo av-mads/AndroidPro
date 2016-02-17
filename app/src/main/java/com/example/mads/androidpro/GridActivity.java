c8cdbd4b46cc8503a967f6fd749b11a2a7800af8
MadsChris92 <madschris888@gmail.com>
2016-02-17 15:49:27 +0100

    grid

--- app/src/main/java/com/example/mads/androidpro/GridActivity.java
+++ app/src/main/java/com/example/mads/androidpro/GridActivity.java
@@ -2,10 +2,12 @@ package com.example.mads.androidpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import java.util.ArrayList;
 
@@ -23,7 +25,14 @@ public class GridActivity extends AppCompatActivity {
         btns.add(1, b1);
 
         TableLayout layout = (TableLayout) findViewById(R.id.tablelayout);
-        
+        TableRow tr1 = new TableRow(this);
+        tr1.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
+        tr1.addView(b1);
+
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
+        tr.addView(b);
+        layout.addView(tr, new TableLayout.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
 
 
 //        ArrayAdapter<Button> adapt = new ArrayAdapter<Button>(this, R.layout.grid_layout, R.id.button, btns);
