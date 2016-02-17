9618da4d213deb96110d92a387a2133451150f2f
MadsChris92 <madschris888@gmail.com>
2016-02-17 16:23:30 +0100

    da

--- app/src/main/java/com/example/mads/androidpro/GridActivity.java
+++ app/src/main/java/com/example/mads/androidpro/GridActivity.java
@@ -2,6 +2,7 @@ package com.example.mads.androidpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,36 +13,73 @@ import android.widget.TableRow;
 import java.util.ArrayList;
 
 public class GridActivity extends AppCompatActivity {
+    final int totalBtns = 9;
+    final int btnsRow = 3;
+    Button[] btn = new Button[9];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_grid);
 //        GridView grid = (GridView) findViewById(R.id.gridView);
         ArrayList<Button> btns = new ArrayList<>();
-        Button b = new Button(this);
-        Button b1 = new Button(this);
-        btns.add(0, b);
-        btns.add(1, b1);
 
-        TableLayout layout = (TableLayout) findViewById(R.id.tablelayout);
-        TableRow tr1 = new TableRow(this);
-        tr1.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
-        tr1.addView(b1);
 
         TableRow tr = new TableRow(this);
         tr.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
-        tr.addView(b);
+
+        TableRow tr1 = new TableRow(this);
+        tr1.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
+
+        TableRow tr2 = new TableRow(this);
+        tr1.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
+
+
+        for (int i = 0; i < totalBtns ; i++) {
+            btn[i] = new Button(this);
+            if(i < btnsRow){
+                tr.addView(btn[i]);
+            }
+            if(i >= btnsRow && i < btnsRow*2){
+                tr1.addView(btn[i]);
+            }
+
+            if(i>= btnsRow*2 && i < btnsRow*3){
+                tr2.addView(btn[i]);
+            }
+        }
+
+        resetField();
+
+
+        TableLayout layout = (TableLayout) findViewById(R.id.tablelayout);
+
         layout.addView(tr, new TableLayout.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
+        layout.addView(tr1, new TableLayout.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
+        layout.addView(tr2, new TableLayout.LayoutParams(AbsListView.LayoutParams.FILL_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
 
 
 //        ArrayAdapter<Button> adapt = new ArrayAdapter<Button>(this, R.layout.grid_layout, R.id.button, btns);
 //        grid.setAdapter(adapt);
 
-        for (int i = 0; i < btns.size(); i++) {
-            btns.get(i).setText("Button " + i);
+    }
 
+    View.OnClickListener clickListen(final Button button){
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                button.setText("BLA");
+            }
+        };
+
+    }
+
+    public void resetField(){
+        for (int i = 0; i < totalBtns; i++) {
+            btn[i].setMinimumWidth(200);
+            btn[i].setText("Button " + (i+1));
+            btn[i].setOnClickListener(clickListen(btn[i]));
         }
-
     }
 }
