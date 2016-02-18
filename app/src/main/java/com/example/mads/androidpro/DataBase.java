bf5f499471fa2527db075a3e098c33aecbc5c255
MadsChris92 <madschris888@gmail.com>
2016-02-18 11:56:12 +0100

    das

--- app/src/main/java/com/example/mads/androidpro/DataBase.java
+++ app/src/main/java/com/example/mads/androidpro/DataBase.java
@@ -1,5 +1,7 @@
 package com.example.mads.androidpro;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,7 @@ public class DataBase extends AppCompatActivity {
     DatabaseHelper myDB;
     EditText editName;
     Button addDataBtn;
+    Button viewAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,10 @@ public class DataBase extends AppCompatActivity {
         setContentView(R.layout.activity_data_base);
         myDB = new DatabaseHelper(this);
         editName = (EditText) findViewById(R.id.input_name);
+        viewAll = (Button) findViewById(R.id.button_view);
         addDataBtn = (Button) findViewById(R.id.button_add);
         addData();
-
-
+        viewAll();
     }
 
     public void addData(){
@@ -30,13 +33,43 @@ public class DataBase extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 boolean isInserted = myDB.insertData(editName.getText().toString());
-                if(isInserted == true){
+                if (isInserted == true) {
                     Toast.makeText(DataBase.this, "Data Inserted", Toast.LENGTH_LONG).show();
-                }
-                else {
+                } else {
                     Toast.makeText(DataBase.this, "Unsuccesful", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+    public void viewAll(){
+        viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor res = myDB.getAllData();
+                if(res.getCount() == 0){
+                    // show message if database is empty
+                    showMessage("Error", "Nothing found");
+                    return;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext()){
+                    buffer.append("id : "+res.getString(0) + "\n");
+                    buffer.append("name : "+res.getString(1) + "\n\n");
+
+                }
+
+                showMessage("Data", buffer.toString());
+            }
+        });
+    }
+
+    public void showMessage(String title, String msg){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(msg);
+        builder.show();
     }
 }
